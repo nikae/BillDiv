b5ccbad9bd5166be61eaac635bb56a634b6dadeb
Nika Elashvili <nika.elashvili@gmail.com>
2016-11-16 13:04:45 -0800

    minor improvements

--- BillDiv/BillDiv/ViewController.swift
+++ BillDiv/BillDiv/ViewController.swift
@@ -148,17 +148,9 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         refreshview()
         
         
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        
-        // Dispose of any resources that can be recreated.
-    }
-    
     
 }
